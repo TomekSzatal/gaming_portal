b0772e3a46935c5066ed38f7cc270df2e7b8fc79
TomekSzatal <szatalowicztomek@gmail.com>
2026-05-20 11:14:31 +0200

    Refactor views and add custom forms support

--- games/views.py
+++ games/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
+from .forms import GameForm, ReviewForm, ArticleForm
 from .models import Game, Article, Review
 
 @login_required
@@ -32,26 +33,25 @@ class GameListView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         query = (self.request.GET.get("q") or "").strip()
-
         if query:
             queryset = queryset.filter(title__icontains=query)
-
         return queryset
 
 
 class GameDetailView(LoginRequiredMixin, generic.DetailView):
     model = Game
+    queryset = Game.objects.prefetch_related("review_set__author")
 
 
 class GameCreateView(LoginRequiredMixin, generic.CreateView):
     model = Game
-    fields = "__all__"
+    form_class = GameForm
     success_url = reverse_lazy("games:game-list")
 
 
 class GameUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Game
-    fields = "__all__"
+    form_class = GameForm
     success_url = reverse_lazy("games:game-list")
 
 
@@ -64,20 +64,28 @@ class ArticleListView(LoginRequiredMixin, generic.ListView):
     model = Article
     paginate_by = 5
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = (self.request.GET.get("q") or "").strip()
+        if query:
+            queryset = queryset.filter(title__icontains=query)
+        return queryset
+
 
 class ArticleDetailView(LoginRequiredMixin, generic.DetailView):
     model = Article
+    queryset = Article.objects.prefetch_related("authors")
 
 
 class ArticleCreateView(LoginRequiredMixin, generic.CreateView):
     model = Article
-    fields = "__all__"
+    form_class = ArticleForm
     success_url = reverse_lazy("games:article-list")
 
 
 class ArticleUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Article
-    fields = "__all__"
+    form_class = ArticleForm
     success_url = reverse_lazy("games:article-list")
 
 
@@ -89,13 +97,16 @@ class ArticleDeleteView(LoginRequiredMixin, generic.DeleteView):
 class ReviewListView(LoginRequiredMixin, generic.ListView):
     model = Review
     paginate_by = 5
+    queryset = Review.objects.select_related("game", "author")
+
 
 class ReviewDetailView(LoginRequiredMixin, generic.DetailView):
     model = Review
 
+
 class ReviewCreateView(LoginRequiredMixin, generic.CreateView):
     model = Review
-    fields = ("game", "rating", "comment")
+    form_class = ReviewForm
     success_url = reverse_lazy("games:review-list")
 
     def form_valid(self, form):
